11bc248104785c20199bb799fc7b22600fc69a56
Akashmame <79107396+Akashmane21@users.noreply.github.com>
2021-09-01 19:05:03 +0530

    Making Home page 2

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -41,6 +41,7 @@ export default function Home() {
     
     <div className="menu">
     <h1>Welcome {UserName} </h1>
+    <img src={Img} alt={UserName} />
 
       <button onClick={logout}>Logout_<svg xmlns="http://www.w3.org/2000/svg" width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M15 3h6v18h-6M10 17l5-5-5-5M13.8 12H3"/></svg></button>
     </div>
@@ -61,6 +62,7 @@ export default function Home() {
             <h3>Phone : </h3>
             <h4>{Phone}</h4>
             </div>
+            <hr />
             
 </div>
 
